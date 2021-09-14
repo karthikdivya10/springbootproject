c32592ae90b54fff8072eafbb379aadfb58b21e1
Karthik <Karthik@Karthik-PC>
2021-09-14 14:43:16 +0530

    WH-8520 First time commit

---
+++ src/main/java/com/example/demo/karthik/MR.java
@@ -0,0 +1,15 @@
+package com.example.demo.karthik;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MR implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("spring");
+
+	}
+
+}
